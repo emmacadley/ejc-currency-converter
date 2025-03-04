238acb4d8d096548a385c854ae9e3f2806090174
emmacadley <emma.cadley@postoffice.co.uk>
2025-03-04 12:08:58 +0000

    Rendering of image library

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {SwapFrom, AED, AUD, CAD, CHF, CNY, EUR, GBP, JPY, NZD, THB, USD, ZAR } from "@/images/ImageLibrary";
 
 export default function Index() {
   return (
@@ -9,7 +10,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to jdjdedit this screen.</Text>
+      <SwapFrom />
+      <AED />
+      <AUD />
+      <CAD />
+      <CHF />
+      <CNY />
+      <EUR />
+      <GBP />
+      <JPY />
+      <NZD />
+      <THB />
+      <USD />
+      <ZAR />
     </View>
   );
 }
